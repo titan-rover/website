3125d5fcb5c8d112a6aa8a3a9ef05b760baeaab6
Khang Pham <khangpham9999@csu.fullerton.edu>
2022-09-01 17:20:12 -0700

    finished the alumni page template and the json files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+// Author: Khang Pham
+// Email: khangpham9999@csu.fullerton.edu
+
+import alumnis2019 from './json_files/alumni/alumni-2019.json';
+import alumnis2018 from './json_files/alumni/alumni-2018.json';
+import alumnis2017 from './json_files/alumni/alumni-2017.json';
+import alumnis2016 from './json_files/alumni/alumni-2016.json';
+import alumnis2015 from './json_files/alumni/alumni-2015.json';
+import alumnis2014 from './json_files/alumni/alumni-2014.json';
+import './css/index.css';
+
+
+let alumniList = [
+    alumnis2019,
+    alumnis2018,
+    alumnis2017,
+    alumnis2016,
+    alumnis2015,
+    alumnis2014
+];
+
+
+// component that generates each individual alumni
+// profile
+function GenerateColumn({alumni}) {
+  let information = alumni.bulletpoints
+    .map( (point, index) => {
+      return (
+        <li key={index}>{point}</li>
+      )
+    });
+  return (
+    <div className="members-col">
+      <div className="img-center">
+        <img src={process.env.PUBLIC_URL +"/images/members/" 
+                  + alumni.img} alt={alumni.name} />
+      </div>
+      <p>{alumni.name}</p>
+      <ul className="center">{information}</ul>
+      
+    </div>
+  )
+}
+
+
+// component that generates a row of three alumni
+// profiles.
+function GenerateRow({alumnis}) {
+  let alumni = alumnis.map((alumni, index) => {
+    return (
+      <GenerateColumn key={index} alumni={alumni} />
+    )
+  });
+  return (
+    <div className="members-row">
+      {alumni}
+      
+    </div>
+  )
+}
+
+// generates all of the alumni profiles in an entire 
+// year.
+
+function GenerateContainer({alumnis}) {
+  let rows = [];
+  let alumniList = [];
+  let count = 0;
+  alumnis.members.forEach((alumni, index) => {
+    alumniList.push(alumni);
+    if ( (index + 1) % 3 === 0) {
+      rows.push(<GenerateRow alumnis={alumniList} key={count}/>);
+      alumniList = [];
+      count++;
+    }
+  });
+  if (alumniList) {
+    count++;
+    rows.push(<GenerateRow alumnis={alumniList} key={count} />);
+    alumniList = [];
+  }
+
+  if (alumnis.honorable_members) {
+    count++;
+    rows.push(<p key={count} className="year-header">Honorable Team Members</p>);
+    alumnis.honorable_members.forEach((alumni, index) => {
+      count++;
+      alumniList.push(alumni);
+      if ((index + 1) % 3 === 0) {
+        rows.push(<GenerateRow alumnis={alumniList} key={count} />);
+        alumniList = [];
+      }
+    });
+
+    if (alumniList) {
+      count++;
+      rows.push(<GenerateRow alumnis={alumniList} key={count} />);
+      alumniList = [];
+    }
+  }
+
+  return (
+    <div className="members-container">
+      {rows}
+    </div>
+  ) 
+}
+
+function GenerateYear({alumnis}) {
+  return (
+    <section className="team-container">
+      <p className="year-header">{alumnis.year} </p>
+      <div className="team-img">
+        <img src={process.env.PUBLIC_URL + "/images/team_photos/" 
+                  + alumnis.image} 
+              alt={alumnis.year+ " Team"}/>
+      </div>
+      <p className="caption">{alumnis.caption}</p>
+      <GenerateContainer alumnis={alumnis} />
+    </section>  
+  )
+}
+
+function GenerateYears({yearList}) {
+  let years = yearList.map( (year, index) => {
+    return (
+      <GenerateYear alumnis={year} key={index} />
+    )
+  });
+  return (
+    <div>
+      {years}
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GenerateYears yearList={alumniList} />
     </div>
+
   );
 }
 
