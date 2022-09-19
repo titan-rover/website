8ed1c09d3e75c7fc69e719cafffc8d2ccadd9be4
Hyudini <81537410+Hyudini@users.noreply.github.com>
2022-09-19 08:57:35 -0700

    Update Sponsors.js
    edited sponsors intro

--- src/routes/Sponsors.js
+++ src/routes/Sponsors.js
@@ -2,13 +2,33 @@
 // Email: khangpham@csu.fullerton.edu
 
 import NavBar from '../components/Navbar';
+import '../css/sponsors.css'
 
-
+function Text({name}){
+	return(
+		<div>
+		<p className="text">{name}</p>
+		</div>
+	)
+}
 function Sponsors() {
 	return(
 		<div>
 			<NavBar />
-			<p>Sponsors page goes here</p>
+			<body>
+			<h1><center><Text name={"Sponsors"} /></center></h1>
+			
+			<p>
+				<center>
+					Titan Rover sincerely thanks all of our generous sponsors for their contributions. 
+					With increasing costs each year, our sponsors help make our success possible. If you are interested in  
+				</center>
+				<center>sponsoring Titan Rover through either financial or gifts-in-kind donations, 
+					please contact Titan Rover at <a href="teamrover@titanrover.com">teamrover@titanrover.com</a>. 
+					Your support makes an impactful difference to our team.</center>
+			
+			</p>
+			</body>
 		</div>
 
 	)
