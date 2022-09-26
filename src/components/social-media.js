e3a30f1073286db6d6d0f6e779e68da012954c2c
emilyk123 <emilykrohn@csu.fulleton.edu>
2022-09-26 15:18:45 -0700

    I fixed the file path for the images.

--- src/components/social-media.js
+++ src/components/social-media.js
@@ -4,28 +4,28 @@ function SocialMediaComponent() {
     return (
         <div id="icon-container">
             <div className="logo">
-                <img src={process.env.PUBLIC_URL + '/images/CSUF_Titans_Logo.svg-300x300.png'}
+                <img src={process.env.PUBLIC_URL + '/images/SocialMediaImages/CSUF_Titans_Logo.svg-300x300.png'}
                alt="" />
             </div>
             <div>
                 <a href="https://www.facebook.com/TitanRover/">
-                    <img src={process.env.PUBLIC_URL + '/images/FacebookLogo.png'}
+                    <img src={process.env.PUBLIC_URL + '/images/SocialMediaImages/FacebookLogo.png'}
                     className="images"alt="" />
                 </a>
                 <a href="https://www.instagram.com/titanrover/?hl=en">
-                    <img src={process.env.PUBLIC_URL + '/images/InstagramLogo.png'}
+                    <img src={process.env.PUBLIC_URL + '/images/SocialMediaImages/InstagramLogo.png'}
                         className="images"alt="" />
                 </a>
                 <a href="https://twitter.com/titan_rover?lang=en">
-                    <img src={process.env.PUBLIC_URL + '/images/TwitterLogo.png'}
+                    <img src={process.env.PUBLIC_URL + '/images/SocialMediaImages/TwitterLogo.png'}
                         className="images"alt="" />
                 </a>
                 <a href="https://www.linkedin.com/company/titan-rover">
-                    <img src={process.env.PUBLIC_URL + '/images/LinkedinLogo.png'}
+                    <img src={process.env.PUBLIC_URL + '/images/SocialMediaImages/LinkedinLogo.png'}
                         className="images"alt="" />
                 </a>
                 <a href="https://www.youtube.com/channel/UCEAnREP_8HXmqSLfOMv6Lww?view_as=subscriber">
-                    <img src={process.env.PUBLIC_URL + '/images/YoutubeLogo.png'}
+                    <img src={process.env.PUBLIC_URL + '/images/SocialMediaImages/YoutubeLogo.png'}
                         className="images"alt="" />
                 </a>
             </div>
