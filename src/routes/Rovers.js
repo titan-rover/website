0f6727af4f593a95bbc591a4e9439940852cfcc1
Justin Kim <justinkim@Justins-MacBook-Pro-4.local>
2022-09-29 00:20:21 -0700

    Created the slideshow component. The component will allow others to create a slideshow of images. Just provide the array of image urls. However, the user needs to adjust the style according to the webpage.

--- src/routes/Rovers.js
+++ src/routes/Rovers.js
@@ -2,34 +2,21 @@
 // Email: khangpham@csu.fullerton.edu, jcykim@csu.fullerton.edu
 import React, {useState, useEffect} from 'react'
 import NavBar from '../components/Navbar';
+import Slideshow from '../components/Slideshow';
+import '../css/rovers.css'
+import rover from '../json_files/rover/rover-2021.json'
 
 function Rovers() {
-	const [data, setData] = useState([]);
-	const getData = () => {
-		fetch('src/json_files/rover/rover-2021.json'
-		,{
-			headers: {
-				'Content-Type': 'application/json',
-				'Accept': 'application/json'
-			}
-		}
-		)
-		.then(function(response){
-			console.log(response);
-			return response.json();
-		})
-		.then(function(myJson){
-			console.log(myJson);
-			setData(myJson);
-		});
-	}
-	useEffect(()=>{
-		getData()
-	},[])
+
+	const rover2021 = rover.images; // Get the array of urls from the json file
+
 	return (
 		<div>
 			<NavBar />
-			<p>Rover page goes here</p>
+			<div classList = "slideContainer">
+				{/* Use array as argument for component */}
+				<Slideshow slides={rover2021} /> 
+			</div>
 		</div>
 	)
 }
