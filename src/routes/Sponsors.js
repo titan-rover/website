ff738a8de581d945fa9eb1f90d156a02c3861e3d
Hyudini <81537410+Hyudini@users.noreply.github.com>
2022-09-29 20:06:26 -0700

    Update Sponsors.js

--- src/routes/Sponsors.js
+++ src/routes/Sponsors.js
@@ -8,17 +8,7 @@ function Sponsors() {
 			<NavBar />
 			<body>
 			<center><h1 className="orange">{"Sponsors"}</h1></center>
-			
-			<p>
-				<center>
-					Titan Rover sincerely thanks all of our generous sponsors for their contributions. 
-					With increasing costs each year, our sponsors help make our success possible. If you are interested in  
-				</center>
-				<center>sponsoring Titan Rover through either financial or gifts-in-kind donations, 
-					please contact Titan Rover at <a href="teamrover@titanrover.com">teamrover@titanrover.com</a>. 
-					Your support makes an impactful difference to our team.</center>
-			
-			</p>
+			<p className="spacing">{'Titan Rover sincerely thanks all of our generous sponsors for their contributions. With increasing costs each year, our sponsors help make our success possible. If you are interested in sponsoring Titan Rover through either financial or gifts-in-kind donations, please contact Titan Rover at '}<a href="teamrover@titanrover.com">teamrover@titanrover.com</a>{'. Your support makes an impactful difference to our team.'}</p>
 			</body>
 		</div>
 
