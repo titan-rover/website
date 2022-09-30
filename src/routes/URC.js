32ef686fe444a6324071220f56a571a326a7a9e6
Khang Pham <khangpham9999@csu.fullerton.edu>
2022-09-30 10:32:20 -0700

    added the URC introduction

--- src/routes/URC.js
+++ src/routes/URC.js
@@ -2,13 +2,30 @@
 // Email: khangpham@csu.fullerton.edu
 
 import NavBar from '../components/Navbar';
-
+import '../css/urc.css';
 
 function URC() {
 	return (
 		<div>
 			<NavBar />
-			<p>URC page goes here</p>
+			<div>
+				<img src={process.env.PUBLIC_URL + "/images/URC/URC-img.jpg"} 
+					 alt="" 
+					 className="image" />
+			</div>
+			<div className="information">
+				
+				<h1 className="header">
+					What is URC?
+				</h1>
+				<p className="information-content">
+					"URC" is an acronym for the Mars Society's internation, collegiate competition, 
+					the University Rover Challenge, that takes place annually near the Mars Desert Research
+					Station. The goal of the challenge is to have students create a mock rover that can one day
+					aid human inhabitants of Mars. There are 4 tasks that each team must complete.
+					Extreme Delivery and Retrival, Equipment Servicing, Autonomuous Traversal, and a Science Mission. 
+				</p>
+			</div>
 		</div>
 	)
 }
