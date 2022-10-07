4c6a2325628cd7f1b0547036cc423cd69202ba1d
siddharthmohite@csu.fullerton.edu <siddharthmohite@csu.fullerton.edu>
2022-10-07 13:46:26 -0700

    Updated slideshow on rovers page

---
+++ src/components/sliderData.js
@@ -0,0 +1,119 @@
+export const SliderData= [
+    {
+        title:'rovers22',
+        image:'https://i.ibb.co/87hG0bw/rovers21-1.png'
+        
+    },
+    {
+        title:'rovers21',
+        image:'https://i.ibb.co/xhmShz5/rovers21-2.jpg'
+        
+    },
+    {
+        title:'rovers21',
+        image:'https://i.ibb.co/Z1QLRzz/Rovers21gif-1.gif'
+        
+    },
+    {
+        title:'kronos1',
+        image:'https://i.ibb.co/df6f6nv/kronos1.jpg'
+    },
+    {
+        title:'kronos1'  ,
+        image:'https://i.ibb.co/j4RDcBd/kronos2.jpg'
+    },
+    {
+        title:'kronos'  ,
+        image:'https://i.ibb.co/NWbJYc6/kronos3.jpg'
+    },
+    {
+        title:'odyssey'  ,
+        image:'https://i.ibb.co/mt6wTgx/odyssey1.jpg'
+    },
+    {
+        title:'odyssey'  ,
+        image:'https://i.ibb.co/L5XwSS3/odyssey2.jpg'
+    },
+    {
+        title:'odyssey'  ,
+        image:'https://i.ibb.co/mvrmbdz/odyssey3.jpg'
+    },
+    {
+        title:'odyssey'  ,
+        image:'https://i.ibb.co/mCMw778/odyssey4.jpg'
+    },
+    {
+        title:'odyssey'  ,
+        image:'https://i.ibb.co/cb5cskQ/odyssey5.jpg'
+    },
+    {
+        title:'calypso'  ,
+        image:'https://i.ibb.co/wWjnyc0/calypso1.png'
+    },
+    {
+        title:'calypso'  ,
+        image:'https://i.ibb.co/GpvF056/calypso2.png'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/f1GvRfy/atlas2.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/YPmfmtH/atlas3.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/r7kjtts/atlas4.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/zmLmCR6/atlas5.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/fvwx2mr/atlas6.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/v3R2Cmw/atlas7.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/1RcWSgn/atlas8.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/rmMZysz/atlas9.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/TmB88js/atlas10.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/n0hBkCN/atlas2017.jpg'
+    },
+    {
+        title:'atlas'  ,
+        image:'https://i.ibb.co/HhFv5ZF/atlas11.jpg'
+    },
+    {
+        title:'titan'  ,
+        image:'https://i.ibb.co/BV6vJ5g/titan1.png'
+    },
+    {
+        title:'astro'  ,
+        image:'https://i.ibb.co/qx9CHfh/titan2.jpg'
+    },
+    {
+        title:'astro'  ,
+        image:'https://i.ibb.co/rfwJ0y3/astro1.jpg'
+    },
+    {
+        title:'ion'  ,
+        image:'https://i.ibb.co/7SGYJGh/ion2014.jpg'
+    }
+
+
+]
--- src/routes/Rovers.js
+++ src/routes/Rovers.js
@@ -2,12 +2,35 @@
 // Email: khangpham@csu.fullerton.edu
 
 import NavBar from '../components/Navbar';
+import ImageSlider from '../components/ImageSlider';
+import {SliderData} from '../components/sliderData';
+import '../components/styles.css'
 
 function Rovers() {
 	return (
 		<div>
 			<NavBar />
-			<p>Rover page goes here</p>
+			<div className='ourRovers'>
+        Our Rovers
+      </div>
+
+      <div className='Rover21'>
+        Rover' 21
+      </div>
+			{/* <ImageSlider slides={SliderData} /> */}
+			<ImageSlider slides={SliderData.filter((slide) => slide.title === 'rovers21')} />
+      <div className='Rover21'>
+        Kronos
+      </div>
+			<ImageSlider slides={SliderData.filter((slide) => slide.title === 'kronos1')} />
+			{/* <ImageSlider slides={SliderData.filter((slide) => slide.title === 'odyssey')} /> */}
+			{/* <ImageSlider slides={SliderData.filter((slide) => slide.title === 'calypso')} /> */}
+			{/* <ImageSlider slides={SliderData.filter((slide) => slide.title === 'atlas')} /> */}
+			{/* <ImageSlider slides={SliderData.filter((slide) => slide.title === 'titan')} /> */}
+			{/* <ImageSlider slides={SliderData.filter((slide) => slide.title === 'astro')} /> */}
+			{/* <ImageSlider slides={SliderData.filter((slide) => slide.title === 'ion')} /> */}
+			{/* <ImageSlider slides={SliderData} /> */}
+			{/* <p>Rover page goes here</p> */}
 		</div>
 	)
 }
