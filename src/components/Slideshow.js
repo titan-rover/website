b55e29eefcab31f00939301e3c63fad02ba6dc13
Khang Pham <khangpham9999@csu.fullerton.edu>
2022-10-20 02:15:18 -0700

    added the rovers to the rovers page

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -5,24 +5,10 @@ import {useState} from "react";
 
 // importing css
 import '../css/slideshow.css';
-import { getSuggestedQuery } from "@testing-library/react";
-
 
 const Slideshow = ({slides}) => {
     const [currentSlideIndex, setSlideIndex] = useState(0);
 
-    const styles = {
-        slide: {
-            width: "300px",
-            height: "300px",
-            borderRadius: "12px",
-            margin: "0 0"
-        },
-        arrow: {
-            fontSize: "2rem",
-            color: "lightgrey"
-        }
-        }
 
     const goLeft = ()=> {
         const newIndex = currentSlideIndex ? currentSlideIndex - 1 : slides.length - 1;
@@ -34,17 +20,23 @@ const Slideshow = ({slides}) => {
         setSlideIndex(newIndex);
     }
     
+    const left_arrow = <div className={"arrow left"} onClick={goLeft}>❮</div>;
+    const right_arrow = <div className={"arrow right"} onClick={goRight}>❯</div>
     return (
-        <div className = "container">
-            <div className="slide">
-                <div style={styles.arrow} onClick={goLeft}>❮</div>
-                <img style = {styles.slide} src={process.env.PUBLIC_URL + `${slides[currentSlideIndex]}`} />
-                <div style={styles.arrow} onClick={goRight}>❯</div>
-            </div>
-            <div className="description">
-                
-            </div>
+        <div className="slide-container">
+
+            {
+                slides.length > 1 && left_arrow
+            }
+            <img className={"slide"} src={process.env.PUBLIC_URL + `${slides[currentSlideIndex]}`}
+                 alt={"slide" + currentSlideIndex}
+            />
+            {
+                slides.length > 1 && right_arrow
+            }    
         </div>
+            
+            
     )
 }
 
