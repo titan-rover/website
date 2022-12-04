e5f5113edd33238066dd5d75a25fc39b7e4855b7
Oyin's Mac <mymac@oyinsssmac.attlocal.net>
2022-12-03 16:40:05 -0800

    added a new file, and changed the navbar, index and navcss file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,40 +3,64 @@
 
 
 // importing react router
-
 import { Link } from "react-router-dom";
 
 // importing css
 import '../css/navbar.css';
 
 
-
-
+import React,{useEffect, useState} from 'react';
+import logoimg  from "./nav-bar-img.png"
 function NavBar() {
-	return (
-		<section>
-			<nav id="nav-bar">
-				<img id="logo"
-					 src={process.env.PUBLIC_URL + 
-						 '/images/nav-bar-img.png'} alt="" />
-				<div className="nav-bar-container">
-					<Link to="/">Homepage</Link>
-					<Link to="/About">About</Link>
-					<Link to="/URC">URC</Link>
-					<Link to="/Rovers">Rovers</Link>
-					<Link to="/Sponsors">Sponsors</Link>
-					<Link to="/Alumni">Alumni</Link>
-					<a href="
-					https://give.fullerton.edu/donate?fid=z2C7%2bB5psrQ%3d&fdesc=CTsbnwTiVZ1TMmzXJGGkBA%3d%3d&fundID=095008-4048&source=21WWG">Donate To Titan Rover</a>
-				</div>
-				<input id="menu-toggle" type="checkbox" />
-               <label class='menu-button-container' for="menu-toggle">
-              <div class='menu-button'></div>
-            </label>
-			</nav>	
-			
-		</section>
-	)
+  const [expanded, setExpanded] = useState(false)
+  const toggleMenu =()=>{
+    setExpanded(!expanded)
+  }
+  useEffect(()=>{
+    if(expanded === true){
+    document.getElementById("expanded").classList.add("expanded")
+    }else{
+      document.getElementById("expanded").classList.remove("expanded")
+    }
+  },[expanded])
+  return (
+    
+      <nav id='nav-bar'>
+        <img
+          id='logo'
+          src={logoimg}
+          alt=''
+        />
+				    <button id ="hamburger" onClick={toggleMenu} className="hamburger">
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+        <div id="expanded" className='nav-bar-container'>
+             <ul>
+                     <li>  <a href='/'>Homepage</a></li>
+         <li> <a href='/About'>About</a></li>
+         <li> <a href='/URC'>URC</a></li>
+        <li>  <a href='/Rovers'>Rovers</a></li>
+        <li>  <a href='/Sponsors'>Sponsors</a></li>
+        <li>  <a href='/Alumni'>Alumni</a></li>
+        <li>  <a href='https://give.fullerton.edu/donate?fid=z2C7%2bB5psrQ%3d&fdesc=CTsbnwTiVZ1TMmzXJGGkBA%3d%3d&fundID=095008-4048&source=21WWG'>Donate to Titan Rover</a></li>
+		
+             </ul>
+        </div>
+      </nav>
+    
+  );
 }
 
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Route, Routes, HashRouter } from "react-router-dom";
 
-import Homepage from './routes/Homepage';
-import About from './routes/About';
-import URC from './routes/URC';
-import Rovers from './routes/Rovers'
-import Sponsors from './routes/Sponsors';
-import Alumni from './routes/Alumni';
+import App  from './App'
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="About" element={<About />}/>
-        <Route path="URC" element={<URC />}/>
-        <Route path="Rovers" element={<Rovers />}/>
-        <Route path="Sponsors" element={<Sponsors />} />
-        <Route path="Alumni" element={<Alumni />} />
-      </Routes>
-    </HashRouter>
-  </React.StrictMode>
-);
+ReactDOM.createRoot( 
+  document.querySelector('#root')
+).render(<App />)
 
